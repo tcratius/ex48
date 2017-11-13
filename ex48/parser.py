b1ac714b56163ec7e4561f2f9347549885be9091
tcratius <tcratius@gmail.com>
2017-11-13 11:24:39 +1000

    removed self

--- ex48/parser.py
+++ ex48/parser.py
@@ -6,73 +6,71 @@ class Sentence(object):
     # need a object to put results in i.e. results
     def __init__(self, subj, verb, obj):
         # remember we take ('noun', 'princess') tuples and convert
-
         self.subj = subj(1)
         self.verb = verb(1)
         self.obj = obj(1)
-        self.word = []
-        self.word_list = []
 
-    def peek(self, word_list):
-        if self.word_list:
-            self.word = self.word_list[0]
-            return self.word[0]
+
+    def peek(word_list):
+        if word_list:
+            word = word_list[0]
+            return word[0]
         else:
             return None
 
-    def match(self, word_list, expecting):
-        if self.word_list:
-            self.word = self.word_list.pop(0)
+    def match(word_list, expecting):
+        if word_list:
+            word = word_list.pop(0)
 
-            if self.word[0] == self.expecting:
-                return self.word
+            if word[0] == expecting:
+                return word
             else:
                 return None
         else:
             return None
 
-    def skip(self, word_list, word_type):
-        while peek(self.word_list) == self.word_type:
-            match(self.word_list, self.word_type)
+    def skip(word_list, word_type):
+        while peek(word_list) == word_type:
+            match(word_list, word_type)
 
 
 
-    def parse_verb(self, word_list):
-        skip(self.word_list, 'stop')
+    def parse_verb(word_list):
+        skip(word_list, 'stop')
 
-        if peek(self.word_list) == 'verb':
-            return match(self.word_list, 'verb')
+        if peek(word_list) == 'verb':
+            return match(word_list, 'verb')
         else:
-            raise ParserError("Expected a verb next.")
+            raise ParserError("Expected a verb next. Received %s ") % word_list
 
 
-    def parse_object(self, word_list):
-        skip(self.word_list, 'stop')
-        self.next_word = peek(self.word_list)
+    def parse_object(word_list):
+        skip(word_list, 'stop')
+        next_word = peek(word_list)
 
-        if self.next_word == 'noun':
+        if next_word == 'noun':
             return match(self.word_list, 'noun')
         elif next_word == 'direction':
-            return match(self.word_list, 'direction')
+            return match(word_list, 'direction')
         else:
-            raise ParseError("Expected a noun or direction next.")
+            raise ParseError("Expected a noun or direction next. Received %s ") % wordlist
 
+    def parse_subject(word_list, subject):
+        self.verb = parse_verb(word_list)
+        self.object = parse_object(word_list)
 
+        return Sentence(subject, verb, object)
 
-    def parse_subject(self, word_list):
-        skip(self.word_list, 'stop')
-        self.next_word = peek(self.word_list)
-
-        if self.next_word == 'noun':
-            return match(self.word_list, 'noun')
-        elif self.next_word == 'verb':
-            return ('noun', 'player')
-        else:
-            raise ParserError("Expected a verb next.")
 
     def parse_sentence(self, word_list):
-        self.subject = parse_subject(self.word_list)
-        self.verb = parse_verb(self.word_list)
-        self.object = parse_object(self.word_list)
+        skip(word_list, 'stop')
+        next_word = peek(word_list)
 
-        return Sentence(self.subject, self.verb, self.object)
+        if next_word == 'noun':
+            subj = match(word_list, 'noun')
+            return parse_subject(word_list, subj)
+        elif next_word == 'verb':
+            # assume the subject is the player then
+            return parse_subject(word_list, ('noun', 'player'))
+        else:
+            raise ParserError("Must start with subject, object, or verb not: %s.") % word_list
