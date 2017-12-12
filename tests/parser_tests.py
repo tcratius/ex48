161466546e0329dc13140cd9d841f75f809da461
Conrad Thiele <tcratius@gmail.com>
2017-12-12 20:31:12 +1000

    Need to fix test skip()

--- tests/parser_tests.py
+++ tests/parser_tests.py
@@ -3,21 +3,32 @@ import pytest
 from ex48 import parser
 from ex48.parser import Sentence
 
-
+@pytest.fixture()
 def test_sentence():
-    sentence = Sentence(("noun","player"),("verb", "go"), ("direction", "north"))
-    assert_equal(sentence('noun', 'player'))
-    assert_equal(sentence('verb', 'go'))
-    assert_equal(sentence('direction', 'north'))
+    sentence = Sentence.parse_sentence([("noun","player"),("verb", "go"),("noun", "bear")])
+    return sentence
+@pytest.fixture()
+def test_skip_word():
+    skip_word_one[0], skip_word_two[1] = ([("stop", "at")]
+    return skip_word_one, skip_word_two
 
+@classmethod
 def test_peek():
-    pass
+    result = test_sentence()
+    assert Sentence.peek(result) != None
 
+@classmethod
 def test_match():
-    pass
+    result = test_setence()
+    expecting = "noun"
+    assert Sentence.match(result, expecting) != None
 
+@classmethod
 def test_skip():
-    pass
+    result1, result2  = test_skip_word()
+    assert Sentence.skip(result1, result2) != None
+
+
 
 def test_parse_verb():
     pass
